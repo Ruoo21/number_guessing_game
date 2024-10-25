983dc6bab6ae42eec9367ca3f82c67984a4d9997
Ruoo21 <143183938+Ruoo21@users.noreply.github.com>
2024-10-25 14:47:48 +0200

    Allow player to input difficulty

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ import random
 print(
     "Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100."
 )
-print("You have 5 chances to guess the correct number.")
+print("You have 5 chances to guess the correct number.\n")
+
+print("1. Easy (10 chances)")
+print("2. Medium (5 chances)")
+print("3. Hard (3 chances)")
+difficulty_level = input("Please select the difficulty level: ")
